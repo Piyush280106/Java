5aa224141cb1ad53a5752b75e91ffb4ebfcdc4dd
Piyush Patil <151504726+Piyush280106@users.noreply.github.com>
2025-07-03 22:01:56 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+
+public class Assignment10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0,a=0,b=1,c;
+		while(i<10)
+		{
+			System.out.println(a);
+			c=a+b;
+			a=b;
+			b=c;
+			i++;
+		}
+	}
+
+}
