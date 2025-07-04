f22c8f9688ce8507b9513829b14c7023f3e38b42
Piyush Patil <151504726+Piyush280106@users.noreply.github.com>
2025-07-04 21:17:25 +0530

    Conversion.java

--- Conversion.java
+++ Conversion.java
@@ -1,6 +1,6 @@
-import java.util.Scanner;
+rsinimport java.util.Scanner;
 import java.util.*;
-public class problem1 {
+public class Conversion {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
